5a4c90e75eebaa037519b7283e99fe86508a656c
unknown <calemamanispe.12@gmail.com>
2023-10-04 23:01:07 -0400

    lab3

---
+++ src/services/models/models.tsx
@@ -0,0 +1,13 @@
+export interface RequestOptions {
+    endpoint: string,
+    method: string,
+    options?: object,
+    body?: any
+    isFile?: boolean
+}
+export interface Post { 
+    userId: 1,
+    id: 2,
+    title: "qui ste esse",
+    body: "est rerum tempore vitae\nsequi sint nihil reprehender"
+}
